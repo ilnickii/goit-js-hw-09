ba61b37947dd8b7e99ad3cf75f31f2be59069ff0
ilnickii <ilnickii2008@gmail.com>
2023-09-17 14:00:54 +0300

    Add task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const delay = Number(event.target.elements.delay.value);
+  const step = Number(event.target.elements.step.value);
+  const amount = Number(event.target.elements.amount.value);
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const promiseDelay = delay + i * step;
+    createPromise(position, promiseDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
