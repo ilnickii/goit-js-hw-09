bf9841e3c451a325a3264a32b16fba193c151d9a
ilnickii <ilnickii2008@gmail.com>
2023-09-17 11:11:39 +0300

    Add task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function changeBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+startButton.addEventListener('click', () => {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  }
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+  intervalId = null;
+  startButton.disabled = false;
+  stopButton.disabled = true;
+});
